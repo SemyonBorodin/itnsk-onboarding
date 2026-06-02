e2b1535fba76d25697905f28bf9a00308059f673
SemyonBorodin <SemyonBorodin@users.noreply.github.com>
2026-06-02 18:52:00 +0700

    symfony documentation: redirecting and url generating

--- src/Controller/TestBlogController.php
+++ src/Controller/TestBlogController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TestBlogController extends AbstractController
@@ -21,12 +22,14 @@ class TestBlogController extends AbstractController
 //        requirements: ['item' => '[0-9]+'],
         methods: ['GET'],
         locale: 'en')]
-    public function item(int $item = 0): ?Response
+    public function item(int $item = 0): Response
     {
         $blog_list = ['blog1', 'blog2'];
 
         if (!isset($blog_list[$item])) {
-            return new Response('Not found', 404);
+//            return new Response('Not found', 404);
+            $url = $this->generateUrl('blog_item', ['item' => 0]);
+            return new RedirectResponse($url);
         }
 
         return new Response(
