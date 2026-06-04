9722f691603289cbbcc62c52d3cb2640fc84dd24
SemyonBorodin <SemyonBorodin@users.noreply.github.com>
2026-06-04 18:26:11 +0700

    blog-main-page: Базовая структура блога
      - Добавлена главная страница с шапкой, подвалом и контентной областью
      - Вынесена общая разметка в base.html.twig
      - Добавлена тестовая страница "О блоге"
      - Добавлены entity Publication и репозиторий
      - Добавлена миграция db для Publication (с помощью php bin/console make:migration)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MainController extends AbstractController
+{
+    #[Route('/main', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController', // пока не используется
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('main/about.html.twig');
+    }
+}
