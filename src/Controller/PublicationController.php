cff3e57b5096df20de391ba02aee97235d3b879e
SemyonBorodin <SemyonBorodin@users.noreply.github.com>
2026-06-08 17:18:34 +0700

    Sonata: в контроллер добавлен маршрут создания публикации, добавлен шаблон создания публикации

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Publication;
+use App\Form\PublicationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class PublicationController extends AbstractController
 {
@@ -13,6 +18,29 @@ final class PublicationController extends AbstractController
     {
         return $this->render('publication/index.html.twig', [
             'controller_name' => 'PublicationController',
+        ]);
+    }
+
+    #[Route('/publication/create', name: 'app_publication_create')]
+    #[IsGranted('ROLE_USER')]
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $publication = new Publication();
+
+        $form = $this->createForm(PublicationType::class, $publication);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($publication);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+        // при первом открытии страницы либо при ошибках формы?
+        return $this->render('publication/create.html.twig', [
+            'form' => $form,
         ]);
     }
 }
